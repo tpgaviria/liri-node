6f999dfcb49f366fee11f225892466f84b347f1c
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-05-03 09:38:23 -0400

    final

--- liri.js
+++ liri.js
@@ -9,10 +9,14 @@ var boxen = require('boxen');
 var fs = require('fs');
 var CFonts = require('cfonts');
 var chalk = require('chalk');
+var wrap = require('wordwrap')(70);
 
 
-// console.log(boxen(``, {padding: {top: 2, bottom: 2, left: 5, right: 5}, margin: {top: 1, bottom: 1}, borderColor: 'cyan', borderStyle: 'double', float: 'center', backgroundColor: '#111', color: '#fff'}));
 
+var command;
+var search;
+var task;
+var doWhat;
 
 // run initial prompt
 
@@ -36,41 +40,45 @@ function prompt() {
 
         // splits input into array
         task = inquirerResponse.task.split(' ');
+        command = task[0];
+        search = task.slice(1).join(' ');
 
-        // sets first word as command
-        var command = task[0];
-
-        //sets all other words as search
-        var search = task.slice(1).join(' ');
-
-        // sets what function is run based on command input
-        switch (command) {
-            case 'concert':
-                bandsInTown(search);
-                break;
-            case 'spotify':
-                spotifySearch(search);
-                break;
-            case 'movie':
-                imdbSearch(search);
-                break;
-            case 'exit':
-                process.exit();
-            default:
-                console.log("I don't know that command. I have failed you. :(");
-                prompt();
-        }
+        doTask(command, search);
 
 
     })
 };
 
-var error = chalk.bgRed;
+function doTask(command, search) {
+
+    switch (command) {
+        case 'concert':
+            bandsInTown(search);
+            break;
+        case 'spotify':
+            spotifySearch(search);
+            break;
+        case 'movie':
+            imdbSearch(search);
+            break;
+        case 'do':
+            doWhatItSays(search);
+            break;
+        case 'exit':
+            process.exit();
+            break;
+        default:
+            console.log(`I don't know what to do with the command "${command}". I have failed you. :(`);
+            prompt();
+    }
+};
+
+
 // searches artists tour dates and locations
 function bandsInTown(search) {
 
     if (!search) {
-        console.log(error('\nPlease enter a band or artist for results.'));
+        CFonts.say('\nPlease enter a band or artist for results.', { font: 'console', align: 'center', space: false });
         prompt();
     } else {
 
@@ -107,34 +115,35 @@ function imdbSearch(search) {
     if (!search) {
 
         search = 'Mr Nobody';
-        console.log("\nYou didn't give me a movie title, so here's one I like.\n");
+        console.log(boxen(wrap("You didn't give me a movie title, so here's one I like.\n\nMr. Nobody\n\n2009 - Belgium, Germany, Canada, France, USA, UK - English, Mohawk\n\nA boy stands on a station platform as a train is about to leave. Should he go with his mother or stay with his father? Infinite possibilites arise from this decision. As long as he doesn't choose, anything is possible.\n\nStarring: Jared Leto, Sarah Polley, Diane Kruger, Linh Dan Pham\n\nIMDB Rating: 7.8/10 / Rotten Tomatoes Rating: 67%"), { padding: { top: 2, bottom: 2, left: 5, right: 5 }, margin: { top: 1, bottom: 1 }, borderColor: 'cyan', borderStyle: 'double', float: 'center', backgroundColor: '#111', color: '#fff' }));
+        promptAgain();
 
-        // prompt();
+    } else {
+
+        var queryURL = 'http://www.omdbapi.com/?apikey=trilogy&t=' + search;
+
+        axios.get(queryURL).then(
+            function (res) {
+
+                var movie = res.data;
+                var title = movie.Title;
+                var year = movie.Year;
+                var imdbRate = movie.Ratings[0].Value;
+                var rottenRate = movie.Ratings[1].Value;
+                var country = movie.Country;
+                var language = movie.Language;
+                var plot = wrap(movie.Plot);
+                var actors = movie.Actors;
+
+
+                console.log(boxen(`${title}\n\n${year} - ${country} - ${language}\n\n${plot}\n\nStarring: ${actors}\n\nIMDB Rating: ${imdbRate} / Rotten Tomatoes Rating: ${rottenRate}`, { padding: { top: 2, bottom: 2, left: 5, right: 5 }, margin: { top: 1, bottom: 1 }, borderColor: 'cyan', borderStyle: 'double', float: 'center', backgroundColor: '#111', color: '#fff' }));
+
+
+                promptAgain();
+
+            }
+        )
     }
-
-    var queryURL = 'http://www.omdbapi.com/?apikey=trilogy&t=' + search;
-
-    axios.get(queryURL).then(
-        function (res) {
-
-            var movie = res.data;
-            var title = movie.Title;
-            var year = movie.Year;
-            var imdbRate = movie.Ratings[0].Value;
-            var rottenRate = movie.Ratings[1].Value;
-            var country = movie.Country;
-            var language = movie.Language;
-            var plot = movie.Plot;
-            var actors = movie.Actors;
-
-
-            console.log(boxen(`${title}\n\n${year} - ${country} - ${language}\n\n${plot}\n\nStarring: ${actors}\n\nIMDB Rating: ${imdbRate} / Rotten Tomatoes Rating: ${rottenRate}`, {padding: {top: 2, bottom: 2, left: 5, right: 5}, margin: {top: 1, bottom: 1}, borderColor: 'cyan', borderStyle: 'double', float: 'center', backgroundColor: '#111', color: '#fff'}));
-
-   
-            promptAgain();
-        }
-
-    )
 
 }
 
@@ -154,10 +163,13 @@ function spotifySearch(search) {
 
         promptAgain();
 
-    }
+    } else if (search === doWhat) {
+        trackSearch(search);
+    } else
+    
+    {
 
 
-    else {
         inquirer.prompt([
             {
                 type: 'list',
@@ -181,7 +193,7 @@ function spotifySearch(search) {
                             var name = artist.name;
                             var genres = artist.genres[0] + ', ' + artist.genres[1];
                             var link = artist.external_urls.spotify;
-                            
+
                             console.log(boxen(`${name}\n\nGenre: ${genres}\n\nHere's a link to the artist on Spotify:\n     ${link}`, { padding: { top: 2, bottom: 2, left: 5, right: 5 }, margin: { top: 1, bottom: 1 }, borderColor: 'cyan', borderStyle: 'double', float: 'center', backgroundColor: '#111', color: '#fff' }));
 
                             promptAgain();
@@ -192,34 +204,61 @@ function spotifySearch(search) {
 
 
                 if (searchtype === 'a Song?') {
-                    spotify.search({
-                        type: 'track',
-                        query: search,
-                        limit: 20
-                    }, function (err, data) {
-                        if (err) {
-                            return console.log('error occurred');
-                        } else {
-
-                            var track = data.tracks.items[0];
-                            var name = track.name;
-                            var album = track.album.name;
-                            var date = track.album.release_date;
-                            var artist = track.artists[0].name;
-                            var link = track.external_urls.spotify;
-
-
-                            console.log(boxen(`"${name}" by ${artist}\n\nFrom the album "${album}"\n     Released ${date}\n\nHere's a link to the song on Spotify:\n     ${link}`, { padding: { top: 2, bottom: 2, left: 5, right: 5 }, margin: { top: 1, bottom: 1 }, borderColor: 'cyan', borderStyle: 'double', float: 'center', backgroundColor: '#111', color: '#fff' }));
-
-
-                            promptAgain();
-
-                        }
-                    })
+                    trackSearch(search);
                 }
-
-
             });
+    }
+}
+
+function trackSearch(search) {
+    spotify.search({
+        type: 'track',
+        query: search,
+        limit: 20
+    }, function (err, data) {
+        if (err) {
+            return console.log('error occurred');
+        } else {
+
+            var track = data.tracks.items[0];
+            var name = track.name;
+            var album = track.album.name;
+            var date = track.album.release_date;
+            var artist = track.artists[0].name;
+            var link = track.external_urls.spotify;
+
+
+            console.log(boxen(`"${name}" by ${artist}\n\nFrom the album "${album}"\n     Released ${date}\n\nHere's a link to the song on Spotify:\n     ${link}`, { padding: { top: 2, bottom: 2, left: 5, right: 5 }, margin: { top: 1, bottom: 1 }, borderColor: 'cyan', borderStyle: 'double', float: 'center', backgroundColor: '#111', color: '#fff' }));
+
+
+            promptAgain();
+
+        }
+    })
+};
+
+function doWhatItSays(search) {
+
+    if (search === 'what it says') {
+        fs.readFile('random.txt', 'utf8', function (error, data) {
+
+            if (error) {
+                return console.log(error);
+            }
+
+
+            var dataArr = data.split(",");
+
+            command = dataArr[0];
+            search = dataArr[1];
+            doWhat = search;
+
+            doTask(command, search);
+
+        });
+    } else {
+        console.log(`I don't know the command "${command} ${search}". I have failed you. :(`);
+        promptAgain();
     }
 }
 
@@ -237,7 +276,7 @@ function promptAgain() {
             } else if (inquirerResponse.another === 'no') {
                 console.log('\n')
                 CFonts.say('goodbye', { font: 'shade', letterSpacing: 2, space: false, align: 'center' });
-                CFonts.say('Have a great day! See you next time!', {font: 'console', align: 'center', space: false});
+                CFonts.say('Have a great day! See you next time!', { font: 'console', align: 'center', space: false });
                 process.exit();
             }
         })
